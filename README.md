d5bcb6944822e01919aaa298edbce880343bc354
iigunchev <iigunchev@gmail.com>
2022-03-22 17:13:29 +0100

    write initial tests

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # express-api-tdd
+
+A simple explanation of TDD (Test Driven Development)
+Step 1 -  you write your tests and they should be failing.
+Step 2 - start implementing until your tests start passing.
+Step 3 - refactor and improve the code. 
+
+On Supertest
+
+It allows you to pull in your express app and simulate calls
--- app.test.js
+++ app.test.js
@@ -1,4 +1,57 @@
+const request = require('supertest');
+const app = require('./app');
 
-it('should run', () => {
-    
+describe('Todos API', () => {
+
+    it('GET /todos --> array todos', () => {
+        return request(app).get('/todos')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then(response => {
+            expect(response.body).toEqual(expect.arrayContaining([
+                expect.objectContaining({
+                    name: expect.any(String),
+                    completed: expect.any(Boolean),
+                })
+            ]))
+        });
+    });
+
+    it('GET /todos/id --> specific todo by id', () => {
+        return request(app).get('/todos/1')
+        .expect('Content-Type', /json/)
+        .expect(200)
+        .then(response => {
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    name: expect.any(String),
+                    completed: expect.any(Boolean),
+                })
+            )
+        });
+    });
+
+    it('GET /todos/id --> 404 if not found', () => {
+        return request(app).get('/todos/9999').expect(404);
+    });
+
+    it('POST /todos --> created todo', () => {
+        return request(app).post('/todos').send({ name: 'go to the gym'})
+        .expect('Content-Type', /json/)
+        .expect(201)
+        .then(response => {
+            expect(response.body).toEqual(
+                expect.objectContaining({
+                    name: 'go to the gym',
+                    completed: false,
+                })
+            )
+        });
+    });
+
+    it('GET /todos --> validate request body', () => {
+        return request(app).post('/todos').send({ name: 123 })
+        .expect(422)
+    });
+
 })
